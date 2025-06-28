79942ce27f7e9a228f2da1c229fdc3141eb95732
Asif Hossain Khan <asif.khan10388@gmail.com>
2025-06-29 03:19:32 +0600

    feat: replace anchor tags with Link components for improved navigation

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 import Line from "@/icons/line/Line";
 
 const Navbar = () => {
   return (
     <header className="relative lg:w-[1080px] lg:mx-[211px] font-[family-name:var(--font-figtree)] flex flex-wrap flex-row justify-center lg:justify-start items-start">
-      <a className="relative w-[107.78px] h-[46.07px] lg:w-[173.1219px] lg:h-[74.0003px]">
+      <Link
+        className="relative w-[107.78px] h-[46.07px] lg:w-[173.1219px] lg:h-[74.0003px]"
+        href="/"
+      >
         <Image
           priority
           fill
@@ -13,7 +17,7 @@ const Navbar = () => {
           src="/logo.svg"
           className="w-full object-cover object-center"
         />
-      </a>
+      </Link>
 
       <div className="absolute top-[26%] right-[8%] lg:hidden flex flex-col items-center justify-center gap-y-[4px]">
         <Line />
@@ -22,8 +26,12 @@ const Navbar = () => {
       </div>
 
       <nav className="lg:ml-auto hidden lg:flex flex-wrap items-center justify-center text-[18px] font-semibold pr-[66px] -mt-[2px]">
-        <a className="text-neutral-n-500 mr-[46px]">About us</a>
-        <a className="text-neutral-n-500">Contact</a>
+        <Link href="/" className="text-neutral-n-500 mr-[46px]">
+          About us
+        </Link>
+        <Link href="/" className="text-neutral-n-500">
+          Contact
+        </Link>
       </nav>
     </header>
   );
